3827f460bd4a4dc12d5af057dffcf9e0f9eb5297
Abhishek Gupta 🎸 <abhishekgupta@infeedo.com>
2022-07-31 21:00:23 +0530

    default first chat open in dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,34 +1,36 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { UserList } from "../../constants/UserList";
+import { Users } from "../../data/Users";
 import ChatList from "../ChatList/ChatList";
 import ChatWindow from "../ChatWindow/ChatWindow";
-const Dashboard = () => {
+import Navbar from "../Navbar/Navbar";
 
-//   const [userId, setUserId] = useState();
-  const { id: userId } = useParams();
-  const navigate = useNavigate();
+
+const user = new Users();
+
+const Dashboard = () => {
+  const userId  = localStorage.getItem("userId");
   const [ selectedUser, setSelectedUser ] = useState("");
   useEffect(() => {
-    if(userId !== localStorage.getItem("userId")) navigate("/login")
+    if (userId && !selectedUser.length) defaultOpenChatByUserId(userId)
   });
 
-  return (
-    userId && typeof userId === "string" ? <div className="container is-widescreen" style={{ height: "100vh", width: "100%" }}>
-      <div style={{ height: "10%" }}>Header Navbar, With settings</div>
-      {/* <div className='is-flex' style={{ height: "80%", width: "100%" }}>
-            <div style={{ width: "20%" }}>
-                Chat List
-            </div>
+  const defaultOpenChatByUserId = (userId: string) => {
+    const userById = user.getUserById(userId);
+    if (userById && userById.chats) {
+      const keysOfUsers = Object.keys(userById.chats)
+      keysOfUsers && keysOfUsers.length ? setSelectedUser(keysOfUsers[0]) : setSelectedUser("")
+    } 
+  }
 
-            <div style={{ width: "80%" }}>
-                Chat Window
-            </div>
-        </div> */}
+  return (
+    userId && typeof userId === "string" ? <div className="container is-widescreen" style={{ height: "100vh" }} >
+      <div style={{ height: "10%" }}><Navbar /></div>
       <div className="columns is-fullheight-with-navbar" style={{ height: "80%" }}>
-        <div className="column is-3 ">
+        <div className="column is-3" style={{ overflowY: "auto" }}>
             <ChatList
-              userId={userId}
-              selectedUser={(user: string) => setSelectedUser(user)}
+              chats={UserList}
+              selectUser={(user: string) => setSelectedUser(user)}
             />
         </div>
         <div className="column">
