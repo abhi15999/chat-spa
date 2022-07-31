42e42278caa1f79a4584d0e284e72d4df63d81e4
Abhishek Gupta 🎸 <abhishekgupta@infeedo.com>
2022-07-31 20:55:17 +0530

    router-changes-in-App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
-// import SignUp from "./components/SignUp/SignUp";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import SignUp from './components/SignUp/SignUp';
+import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+import PrivateRoutes from "./components/HOC/PrivateRoutes";
+import "./App.css"
 function App() {
   return (
     <div className="App">
-      <h2>Chat SPA</h2>
+      <Router>
+        <Routes>
+          <Route path='/' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="/dashboard" element={<PrivateRoutes Component={Dashboard} />} />
+        </Routes>        
+      </Router>
     </div>
     
   );
