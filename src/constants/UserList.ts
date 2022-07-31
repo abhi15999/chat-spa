41235b04a16ee8d910202ffa1ab32fec250b8a0b
Abhishek Gupta 🎸 <abhishekgupta@infeedo.com>
2022-07-31 21:02:13 +0530

    static user list

---
+++ src/constants/UserList.ts
@@ -0,0 +1,30 @@
+export const UserList = [
+    {
+        userId: "1002",
+        name: "Dummy 1"
+    },
+    {
+        userId: "1003",
+        name: "Dummy 2"
+    },
+    {
+        name: "Led Zeppelin",
+        userId: "3acqkai8h1c"
+    },
+    {
+        name: "Metallica",
+        userId: "x0hr1yipoza"
+    },
+    {
+        name: "AC/DC",
+        userId: "8ac41j62qrc"
+    },
+    {
+        name: "Iron Maiden",
+        userId: "mcvt6ky23hp"
+    },
+    {
+        name: "Pink Floyd",
+        userId: "33q4g9bipxv"
+    }
+]
